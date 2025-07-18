453db0aefa7c1dff947bc0872b33240e7c9ae890
Arpit Bhatia <arpitbhatia903@gmail.com>
2025-07-19 02:42:37 +0530

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import Hero from '@/components/Hero';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-// import Navbar from '../components/Navbar';
 
 export default function Layout({ }) {
   return (
